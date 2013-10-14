718dc7ef8fd79c02fe945d965a108f0ac52b8a18
Joseph Leybovich <joseph.leybovich@gmail.com>
2013-10-14 14:28:32 -0700

    commented out teststests

--- Scripts/TestTests.cs
+++ Scripts/TestTests.cs
@@ -1,47 +1,47 @@
-﻿using UnityEngine;
-using System.Collections;
-using System;
-using UnTested;
-
-[AssemblyFixture]
-public class AssemblyTests {
-
-	[AssemblySetup]
-	public IEnumerator TestAssemblySetup ()
-	{
-		yield return new WaitForSeconds (2.0f);
-		Assert.IsNull (null, "THIS SHOULD PASS");
-	}
-
-	[AssemblyTeardown]
-	public IEnumerator TestAssemblyTeardown ()
-	{
-		yield return new WaitForSeconds (2.0f);
-		Assert.IsNull (null, "THIS SHOULD PASS");
-	}
-}
-
-[TestFixture]
-public class TestTests {
-
-	[TestSetup]
-	public IEnumerator TestSetup ()
-	{
-		yield return new WaitForSeconds (2.0f);
-		Assert.IsNull (null, "THIS SHOULD PASS");
-	}
-
-	[Test]
-	public IEnumerator Test ()
-	{
-		yield return new WaitForSeconds (2.0f);
-		Assert.IsNull (null, "THIS SHOULD PASS");
-	}
-
-	[TestTeardown]
-	public IEnumerator TestTeardown ()
-	{
-		yield return new WaitForSeconds (2.0f);
-		Assert.IsNull (null, "THIS SHOULD Pass");
-	}
-}
+﻿//using UnityEngine;
+//using System.Collections;
+//using System;
+//using UnTested;
+//
+//[AssemblyFixture]
+//public class AssemblyTests {
+//
+//	[AssemblySetup]
+//	public IEnumerator TestAssemblySetup ()
+//	{
+//		yield return new WaitForSeconds (2.0f);
+//		Assert.IsNull (null, "THIS SHOULD PASS");
+//	}
+//
+//	[AssemblyTeardown]
+//	public IEnumerator TestAssemblyTeardown ()
+//	{
+//		yield return new WaitForSeconds (2.0f);
+//		Assert.IsNull (null, "THIS SHOULD PASS");
+//	}
+//}
+//
+//[TestFixture]
+//public class TestTests {
+//
+//	[TestSetup]
+//	public IEnumerator TestSetup ()
+//	{
+//		yield return new WaitForSeconds (2.0f);
+//		Assert.IsNull (null, "THIS SHOULD PASS");
+//	}
+//
+//	[Test]
+//	public IEnumerator Test ()
+//	{
+//		yield return new WaitForSeconds (2.0f);
+//		Assert.IsNull (null, "THIS SHOULD PASS");
+//	}
+//
+//	[TestTeardown]
+//	public IEnumerator TestTeardown ()
+//	{
+//		yield return new WaitForSeconds (2.0f);
+//		Assert.IsNull (null, "THIS SHOULD Pass");
+//	}
+//}
